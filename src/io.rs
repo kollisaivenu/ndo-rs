ddef15fde0846e14769ab3593012ec351507b75d
kollisaivenu <ksaivenu2010@gmail.com>
2025-12-26 22:01:06 -0700

    removing self loops in sparse matrice that is read

--- src/io.rs
+++ src/io.rs
@@ -1,16 +1,24 @@
 use std::path::Path;
+use faer::complex::ComplexFloat;
 use sprs::io::{read_matrix_market, IoError};
+use sprs::{TriMat, TriMatI};
 use crate::graph::Graph;
 
 /// Read a matrix market file and output Graph struct.
 pub fn read_matrix_market_as_graph(file_path: &Path) -> Result<Graph, IoError> {
     // read the matrix market file as a TriMat with edge lengths.
-    let tri_mat = read_matrix_market(file_path);
+    let mut trimatrix : Result<TriMatI<f64, usize>, IoError> = read_matrix_market(file_path);
 
-    match tri_mat {
-        Ok(tri_matrix) => {
-            // Read was successful, we return it after converting to CSR.
-            let csr_matrix = tri_matrix.to_csr();
+    match trimatrix {
+        Ok(trimatrix) => {
+            // Read was successful, we return it after converting
+            let mut trimatrix_without_self_loops = TriMat::new((trimatrix.rows(), trimatrix.cols()));
+            for (&edge, (vertex1, vertex2)) in trimatrix.triplet_iter() {
+                if vertex1 != vertex2 {
+                    trimatrix_without_self_loops.add_triplet(vertex1, vertex2, edge);
+                }
+            }
+            let csr_matrix = trimatrix_without_self_loops.to_csr();
             Ok(Graph {
                 graph_csr: csr_matrix,
             })
