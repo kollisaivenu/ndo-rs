f79829dff87f2f13e009946e3a936a71841884c6
kollisaivenu <ksaivenu2010@gmail.com>
2025-12-28 21:06:49 -0700

    Minor fixes to readme and main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::path::Path;
 use ndo_rs::io::read_matrix_market_as_graph;
 use ndo_rs::algorithms::nested_dissection_ordering::NestedDissectionOrdering;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = "./testdata/bcsstm12.mtx";
+    let file_path = "./testdata/bcsstk15.mtx";
     let graph = read_matrix_market_as_graph(Path::new(file_path))?;
-    let ordering = NestedDissectionOrdering {..Default::default()}.compute_ordering(&graph);
+    let (ordering, inverser_ordering) = NestedDissectionOrdering {..Default::default()}.compute_ordering(&graph);
     println!("{:?}", ordering);
     Ok(())
 }
